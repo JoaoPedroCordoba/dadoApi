cc85404bea5090d1c06a859c4a9392c44fb011d0
JoaoPedroCordoba <e107ti@etec.sp;gov;br>
2022-08-11 21:28:06 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/dado/d{numerodefaces}", (
+    [FromRoute] int numerodefaces
+) => {
+
+    if(numerodefaces <= 0){
+
+        return Results.BadRequest(new { mensagem = "Somente dados com pelo menos uma face"});
+
+    };
+
+    int face = RandomNumberGenerator.GetInt32(1, numerodefaces + 1);
+    
+    return Results.Ok(new {dado = $"d{numerodefaces}", rolagem = face});
+}
+);
+
+app.Run();
